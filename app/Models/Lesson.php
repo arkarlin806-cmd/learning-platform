e444ae24688ffb437058ac8d0c8ef3f55455c5e7
ArKar Lin <arkarlin806@gmail.com>
2026-08-09 18:14:42 +0630

    Fix Lesson model filename casing

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lesson extends Model
 {
-    protected $table = 'lessons';
+    // protected $table = 'lessons';
     protected $fillable = [
 
         'course_id',

ede605b193b14ace07df75916bdd5ef97dee7aff
ArKar Lin <arkarlin806@gmail.com>
2026-08-10 14:55:03 +0630

    Fix Railway HTTPS proxy

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -7,12 +7,6 @@ use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
 {
-    /**
-     * The trusted proxies for this application.
-     *
-     * @var array<int, string>|string|null
-     */
-    // protected $proxies;
     protected $proxies = '*';
 
     protected $headers =

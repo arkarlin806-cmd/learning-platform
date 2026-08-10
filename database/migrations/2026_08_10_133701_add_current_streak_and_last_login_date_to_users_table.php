093cfecdaa26982e77608b99b5aca4fb4b5cf4c0
ArKar Lin <arkarlin806@gmail.com>
2026-08-10 13:41:40 +0630

    Add user streak columns migration

--- database/migrations/2026_08_10_133701_add_current_streak_and_last_login_date_to_users_table.php
+++ database/migrations/2026_08_10_133701_add_current_streak_and_last_login_date_to_users_table.php
@@ -6,24 +6,29 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedInteger('current_streak')->default(0);
-            $table->date('last_login_date')->nullable();
+            if (!Schema::hasColumn('users', 'current_streak')) {
+                $table->unsignedInteger('current_streak')->default(0);
+            }
+
+            if (!Schema::hasColumn('users', 'last_login_date')) {
+                $table->date('last_login_date')->nullable();
+            }
         });
     }
 
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn([
-                'current_streak',
-                'last_login_date',
-            ]);
+            if (Schema::hasColumn('users', 'current_streak')) {
+                $table->dropColumn('current_streak');
+            }
+
+            if (Schema::hasColumn('users', 'last_login_date')) {
+                $table->dropColumn('last_login_date');
+            }
         });
     }
 };

0d98921498c05e455a347396510d2e704f8c157c
ArKar Lin <arkarlin806@gmail.com>
2026-08-10 15:36:26 +0630

    Fix Tailwind CSS build

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
+    plugins:[
         laravel({
-            input: [
+            input:[
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
-            refresh: true,
+            refresh:true,
         }),
     ],
-
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        emptyOutDir: true,
-    },
 });
 // import { defineConfig } from 'vite';
 // import laravel from 'laravel-vite-plugin';

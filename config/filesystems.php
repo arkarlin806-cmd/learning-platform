6b6165f9e7d6c90418140df3fd62573abc40a1e1
ArKar Lin <arkarlin806@gmail.com>
2026-08-13 12:16:29 +0630

    Fix Tailwind CSS build

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,18 +60,17 @@ return [
 
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
-
             'region' => env('AWS_DEFAULT_REGION'),
-
             'bucket' => env('AWS_BUCKET'),
-
             'url' => env('AWS_URL'),
-
             'endpoint' => env('AWS_ENDPOINT'),
 
-            'use_path_style_endpoint' => true,
+            'use_path_style_endpoint' => false,
 
             'signature_version' => 'v4',
+
+            'request_checksum_calculation' => 'when_required',
+            'response_checksum_validation' => 'when_required',
 
             'throw' => true,
         ],

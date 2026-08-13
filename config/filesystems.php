360a05636543309a7bea73af3a603269a856d2bd
ArKar Lin <arkarlin806@gmail.com>
2026-08-14 00:29:52 +0630

    Use Backblaze B2 native API for course thumbnails

--- config/filesystems.php
+++ config/filesystems.php
@@ -76,6 +76,21 @@ return [
             'request_checksum_calculation' => 'when_required',
             'response_checksum_validation' => 'when_required',
         ],
+        'b2' => [
+            'driver' => 's3',
+
+            'key' => env('B2_KEY_ID'),
+            'secret' => env('B2_APPLICATION_KEY'),
+
+            'region' => env('B2_REGION'),
+            'bucket' => env('B2_BUCKET'),
+
+            'endpoint' => env('B2_ENDPOINT'),
+
+            'use_path_style_endpoint' => true,
+
+            'throw' => true,
+        ],
 
     ],
 

c6d8732c62c6877d5b9dbc77df9d3efb03c4102c
ArKar Lin <arkarlin806@gmail.com>
2026-08-20 00:17:31 +0630

    Use Backblaze B2 native API for course thumbnails

--- resources/views/layout/user.blade.php
+++ resources/views/layout/user.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{csrf_token()}}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css','resources/js/app.js'])
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.6.0/fonts/remixicon.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
